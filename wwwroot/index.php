64e68dcdd4af4c5a84364bcbdf4d80a4204e67ac
osawa-koki <github@osawa.dev>
2023-01-10 05:37:45 +0900

    トップページ。

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <?php
-$title = "🐙 PHP-on-Docker 🐙";
+$title = "🐙 Fractal-Drawer 🐙";
 ?>
 <html lang="ja">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><?php echo $title; ?></title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <h1><?php echo $title; ?></h1>
-  <?php
-  $comment = "Hello, PHP-on-Docker 🐙🐙🐙!";
-  for ($i = 0; $i < 3; $i++) {
-    echo "<p>$comment</p>";
-  }
-  ?>
+  <main>
+    <h1><?php echo $title; ?></h1>
+    <h2>🐬 マンデルブロ集合 🐬</h2>
+    <iframe src="/canvas/mandelbrot"></iframe>
+    <h2>🐍 ジュリア集合 🐍</h2>
+    <iframe src="/canvas/julia"></iframe>
+  </main>
 </body>
 </html>
