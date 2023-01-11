54bf5cad19a8462e7a30bba3920fa7fe2a648373
osawa-koki <github@osawa.dev>
2023-01-11 22:45:55 +0900

    小さな修正。

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -18,26 +18,31 @@ $title = "🐙 Fractal-Drawer 🐙";
       array(
         'emoji' => '🐬',
         'name' => 'マンデルブロ集合',
+        'description' => "The フラクタル図形ですね🐧\n不思議な感じがするキレイな図形です。",
         'path' => '/canvas/mandelbrot',
       ),
       array(
         'emoji' => '🐍',
         'name' => 'ジュリア集合',
+        'description' => "人気度はマンデルブロ集合に劣るものの、\nマンデルブロ集合に比べて、より複雑な図形でゾワゾワします。",
         'path' => '/canvas/julia',
       ),
       array(
         'emoji' => '🐋',
         'name' => 'トライコーン集合',
+        'description' => "マンデルブロ集合の逆のです。\n逆というのは補集合の意味です。",
         'path' => '/canvas/tricorn',
       ),
       array(
         'emoji' => '🐢',
         'name' => 'バーニングシップ集合',
+        'description' => "なんだか、船が燃えているような図形です。",
         'path' => '/canvas/burning-ship',
       ),
     );
     foreach ($fractals as $fractal) {
       echo '<h2>' . $fractal['emoji'] . ' ' . $fractal['name'] . ' ' . $fractal['emoji'] . '</h2>';
+      echo '<p>' . $fractal['description'] . '</p>';
       echo '<iframe src="' . $fractal['path'] . '"></iframe>';
     }
     ?>
